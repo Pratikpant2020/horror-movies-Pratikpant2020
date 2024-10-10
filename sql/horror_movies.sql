fa6f9454ca3306863af3ed9d4edfa435d426b0c6
bhatt <bhatt.meena@yahoo.com>
2024-10-10 14:01:48 -0400

    horror-movies

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,13 @@
--- Add your SQL here
+SELECT 
+    id AS Movie_ID, 
+    name AS Movie_Title, 
+    imdb_rating AS Rating
+FROM 
+    movies
+WHERE 
+    genre = 'Horror' 
+    AND release_year <= 1985
+ORDER BY 
+    imdb_rating DESC
+LIMIT 3;
+horror_movies.sql to_csv(movies.csv)
